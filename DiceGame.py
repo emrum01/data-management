d5ded1341eaf468cdb117c745d196b865f42dede
emrum01 <emrum01@gmail.com>
2021-07-06 10:16:07 +0900

    Greetings added

--- DiceGame.py
+++ DiceGame.py
@@ -3,6 +3,11 @@ import random
 dice1 = random.randrange(6) + 1
 dice2 = random.randrange(6) + 1
 total = dice1 + dice2
+
+print("What is your name?")
+
+yourName = input()
+print("Hello, {}!".format(yourName))
 print("Rolling the dice...")
 print("Dice 1: {}\nDice 2: {}".format(dice1, dice2))
 print("Total value: {}".format(total))
